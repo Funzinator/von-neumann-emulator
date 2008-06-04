dd659c067fe601fc1a0cf761230709c48bd94a81
Jay <sebastian.jakobi@informatik.uni-giessen.de>
2008-06-04 17:32:28 +0000

    ENI implementiert \ am Zeilenende entfernt
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@17 06a71e81-df49-0410-adf8-df517167f62f

---
+++ src/ENI.cpp
@@ -0,0 +1,11 @@
+#include "ENI.h"
+
+ENI::ENI(RawOperation *rawOp) : IndexregisterOperation(rawOp)
+{
+    this->Param2->setInt(rawOp->Param2.toInt());
+}
+
+void ENI::run(Configuration *c)
+{   //ENI A,B belegt das Indexregister A mit dem Int-Wert B
+    c->getIndexRegister(this->Param1->getInt())->setInt(this->Param2->getInt());
+}
--- src/ENIA.cpp
+++ src/ENIA.cpp
@@ -10,15 +10,15 @@ void ENIA::run(Configuration *c)
     switch (c->getAC()->getType())
     {
         case StorageCell::Binary:
-            c->getIndexRegister(this->Param1->getInt())->setBinary(\
+            c->getIndexRegister(this->Param1->getInt())->setBinary(
                 c->getAC()->getBinary());
             break;
         case StorageCell::Integer:
-            c->getIndexRegister(this->Param1->getInt())->setInt(\
+            c->getIndexRegister(this->Param1->getInt())->setInt(
                 c->getAC()->getInt());
             break;
         case StorageCell::Float:
-            c->getIndexRegister(this->Param1->getInt())->setFloat(\
+            c->getIndexRegister(this->Param1->getInt())->setFloat(
                 c->getAC()->getFloat());
             break;
     }

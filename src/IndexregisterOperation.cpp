2d88d776ac8c9b2f5f7837ccd69e42ef3becf25b
Jay <sebastian.jakobi@informatik.uni-giessen.de>
2008-06-09 22:09:10 +0000

    IndirectOperation: hat nun Prozedur um zur Laufzeit den indirekt adressierten Wert zu ermitteln.
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@18 06a71e81-df49-0410-adf8-df517167f62f

--- src/IndexregisterOperation.cpp
+++ src/IndexregisterOperation.cpp
@@ -3,5 +3,6 @@
 IndexregisterOperation::IndexregisterOperation(RawOperation *rawOp) : UnaryOperation(rawOp)
 {
 	this->Param1->setInt(rawOp->Param1.toInt());
-	/* Param1 ist die Nummer eines IR, deshalb Integer */
+	/* Param1 ist die Nummer eines IR oder einer Speicherzelle,
+	   deshalb Integer */
 }

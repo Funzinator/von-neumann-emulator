deb9e0733690523ffcf5edf97909462e9bf3e0ea
Jay <sebastian.jakobi@informatik.uni-giessen.de>
2008-06-09 22:09:10 +0000

    IndirectOperation: hat nun Prozedur um zur Laufzeit den indirekt adressierten Wert zu ermitteln.
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@18 06a71e81-df49-0410-adf8-df517167f62f

--- include/IndirectOperation.h
+++ include/IndirectOperation.h
@@ -7,6 +7,7 @@ class IndirectOperation : public IndexregisterOperation
 {
 public:
     IndirectOperation(RawOperation*);
+    void IndirectToDirect();
 };
 
 #endif /*INDIRECTOPERATION_H_*/
--- src/IndirectOperation.cpp
+++ src/IndirectOperation.cpp
@@ -3,3 +3,12 @@
 IndirectOperation::IndirectOperation(RawOperation *rawOp) : IndexregisterOperation(rawOp)
 {
 }
+
+void IndirectOperation::IndirectToDirect()
+{
+    if (this->indirect){
+    //überführe "OP,I x" in "OP Inh(x)"
+        this->Param1->setInt(c->Data[this->Param1->getInt()]->getInt());
+        this->indirect=false; //just to be clean
+    }
+}

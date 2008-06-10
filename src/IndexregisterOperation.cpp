c276d116acc9d0ad92e08e49a9e66417d135d2fc
Jay <sebastian.jakobi@informatik.uni-giessen.de>
2008-06-10 20:28:54 +0000

    Um Param1 zu setzen erst eine neue StorageCell anlegen! (habe ich vergessen)
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@34 06a71e81-df49-0410-adf8-df517167f62f

--- src/IndexregisterOperation.cpp
+++ src/IndexregisterOperation.cpp
@@ -2,6 +2,7 @@
 
 IndexregisterOperation::IndexregisterOperation(RawOperation *rawOp) : UnaryOperation(rawOp)
 {
+        this->Param1 = new StorageCell;
 	this->Param1->setInt(rawOp->Param1.toInt());
 	/* Param1 ist die Nummer eines IR oder einer Speicherzelle,
 	   deshalb Integer */

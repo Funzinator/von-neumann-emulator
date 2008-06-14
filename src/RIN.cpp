9518d5898c6e1cac3a5edac87969e0b5f5241eda
Funzinator <github@funzi.de>
2008-06-14 22:11:15 +0000

    DVA, FDV, RIN: Exceptions implementiert (senden HLT-Signal mit Fehlermeldung)
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@63 06a71e81-df49-0410-adf8-df517167f62f

--- src/RIN.cpp
+++ src/RIN.cpp
@@ -10,6 +10,8 @@ void RIN::run(Configuration *c)
     if ((int) c->getAC()->getFloat() - c->getAC()->getFloat() == 0)
         c->getAC()->setInt( (int) c->getAC()->getFloat() );
     else
-        ;//Exception
+    {
+        c->getInterface()->sendSignal(CommunicationInterface::HLT, "Umwandlung fehlgeschlagen");
+    }
     c->setPC(c->getPC() +1);
 }

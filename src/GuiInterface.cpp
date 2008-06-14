b748513788647c30ffc6ef0676fbdc2ce3661662
Funzinator <github@funzi.de>
2008-06-14 02:56:50 +0000

    Gui-Interface-Rumpf erstellt und in MainWindow eingebunden
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@55 06a71e81-df49-0410-adf8-df517167f62f

---
+++ src/GuiInterface.cpp
@@ -0,0 +1,28 @@
+#include "GuiInterface.h"
+
+#include <iostream>
+
+GuiInterface::GuiInterface() : CommunicationInterface()
+{
+    
+}
+
+void GuiInterface::sendSignal(unsigned char)
+{
+    
+}
+
+void GuiInterface::sendSignal(unsigned char, QString)
+{
+    
+}
+
+void GuiInterface::sendString(QString message)
+{
+
+}
+
+QString GuiInterface::receiveString()
+{
+
+}

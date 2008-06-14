e658965a72b100859562a312743050120ba94296
Funzinator <github@funzi.de>
2008-06-14 21:51:26 +0000

    OUI implementiert
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@61 06a71e81-df49-0410-adf8-df517167f62f

---
+++ src/OUI.cpp
@@ -0,0 +1,14 @@
+#include "OUI.h"
+
+OUI::OUI(RawOperation *rawOp) : OutputOperation(rawOp)
+{
+    /* nichts zu tun */
+}
+
+void OUI::run(Configuration *c)
+{
+    QString tmp = "%1";
+    c->getInterface()->sendString(tmp.arg(c->getAC()->getInt()));
+    
+    c->setPC(c->getPC() + 1);
+}

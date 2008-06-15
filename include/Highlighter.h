ff45f02cac3de45613330880003fa15a9be221b6
Funzinator <github@funzi.de>
2008-06-15 23:12:40 +0000

    Highlighter über Textfeld gelegt (ist noch lange nicht komplett!) Bisher werden gültige Zeilen Befehle einfach grün eingefärbt Änderungen am Quelltext haben keinerlei Auswirkungen. Speichern geht auch noch nocht ;-)
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@69 06a71e81-df49-0410-adf8-df517167f62f

---
+++ include/Highlighter.h
@@ -0,0 +1,35 @@
+#ifndef HIGHLIGHTER_H_
+#define HIGHLIGHTER_H_
+
+#include <QSyntaxHighlighter>
+
+class Highlighter : public QSyntaxHighlighter
+{
+    Q_OBJECT
+
+public:
+    Highlighter(QTextDocument *parent = 0);
+
+protected:
+    void highlightBlock(const QString &text);
+
+private:
+    struct HighlightingRule
+    {
+        QRegExp pattern;
+        QTextCharFormat format;
+    };
+    QVector<HighlightingRule> highlightingRules;
+
+    QRegExp commentStartExpression;
+    QRegExp commentEndExpression;
+
+    QTextCharFormat keywordFormat;
+    QTextCharFormat classFormat;
+    QTextCharFormat singleLineCommentFormat;
+    QTextCharFormat multiLineCommentFormat;
+    QTextCharFormat quotationFormat;
+    QTextCharFormat functionFormat;
+};
+
+#endif /*HIGHLIGHTER_H_*/

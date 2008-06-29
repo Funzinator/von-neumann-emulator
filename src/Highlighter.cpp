f4969d499a9500b820768d4904102879dc300308
Funzinator <github@funzi.de>
2008-06-29 16:00:37 +0000

    Highlighter: Hightlighting vorerst abgeschlossen
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@76 06a71e81-df49-0410-adf8-df517167f62f

--- src/Highlighter.cpp
+++ src/Highlighter.cpp
@@ -4,8 +4,51 @@ Highlighter::Highlighter(QTextDocument *parent) : QSyntaxHighlighter(parent)
 {
     HighlightingRule rule;
 
+    /* Befehle (allgemein, unvalidiert) */
+    classFormat.setForeground(Qt::darkGray);
+    classFormat.setFontWeight(QFont::Bold);
+    classFormat.setFontItalic(false);
+    rule.pattern = QRegExp("(NGA|FNG|NOT|INR|INI|INB|OUR|OUI|OUB|IRE|RIN|ENTIER|NOP|STP|HLT|SRR|ENA|ENAR|ENAI|ENIA|ADI|SBI|ENI|LDA|STA|ADA|SBA|MUA|DVA|MDA|FAD|FSB|FMU|FDV|AND|OR|XOR|UJP|SRJ|AZJ,(GR|GE|LS|LE|EQ|NE))");
+    rule.format = classFormat;
+    highlightingRules.append(rule);
+
+    /* Befehle (korrekt) */
+    classFormat.setForeground(Qt::black);
+    classFormat.setFontWeight(QFont::Bold);
+    classFormat.setFontItalic(false);
+    rule.pattern = QRegExp("((NGA|FNG|NOT|INR|INI|INB|OUR|OUI|OUB|IRE|RIN|ENTIER|NOP|STP|HLT|SRR)|((ENA\\s+-?[0-9]+)|(ENAR\\s+-?([0-9]+(\\.[0-9]*)?|\\.[0-9]+)))|((ENAI|ENIA|ADI|SBI)(,I)?\\s+[0-9]+)|(ENI\\s+[0-9]+\\s*,\\s*[0-9]+)|((LDA|STA|ADA|SBA|MUA|DVA|MDA|FAD|FSB|FMU|FDV|AND|OR|XOR|UJP|AZJ,GR|AZJ,GE|AZJ,LS|AZJ,LE|AZJ,EQ|AZJ,NE|SRJ)((,I)?\\s+[0-9]+|\\s+[0-9]+\\s*,\\s*[0-9]+)))\\s*(\\{([^\\}])*\\}\\s*)*$");
+    rule.format = classFormat;
+    highlightingRules.append(rule);
+
+    /* Kommentare */
+    classFormat.setForeground(Qt::gray);
+    classFormat.setFontItalic(true);
+    classFormat.setFontWeight(QFont::Normal);
+    rule.pattern = QRegExp("(\\{([^\\}])*\\}?\\s*)$");
+    rule.format = classFormat;
+    highlightingRules.append(rule);
+
+    /* Integer-Zahlen */
     classFormat.setForeground(Qt::darkGreen);
-    rule.pattern = QRegExp("^\\s*([0-9]+)\\s*:\\s*((NGA|FNG|NOT|INR|INI|INB|OUR|OUI|OUB|IRE|RIN|ENTIER|NOP|STP|HLT|SRR)|((ENA\\s+-?[0-9]+)|(ENAR\\s+-?[0-9]*(\\.[0-9]+)?))|((ENAI|ENIA|ADI|SBI)(,I)?\\s+[0-9]+)|(ENI\\s+[0-9]+\\s*,\\s*[0-9]+)|((LDA|STA|ADA|SBA|MUA|DVA|MDA|FAD|FSB|FMU|FDV|AND|OR|XOR|UJP|AZJ,GR|AZJ,GE|AZJ,LS|AZJ,LE|AZJ,EQ|AZJ,NE|SRJ)((,I)?\\s+[0-9]+|\\s+[0-9]+\\s*,\\s*[0-9]+)))\\s*(\\{([^\\}])*\\}\\s*)*$");
+    classFormat.setFontItalic(false);
+    classFormat.setFontWeight(QFont::Normal);
+    rule.pattern = QRegExp("-?[0-9]+");
+    rule.format = classFormat;
+    highlightingRules.append(rule);    
+
+    /* Float-Zahlen */
+    classFormat.setForeground(Qt::darkGreen); // gleiche Farbe, wie Integer... könnte man aber ändern
+    classFormat.setFontItalic(false);
+    classFormat.setFontWeight(QFont::Normal);
+    rule.pattern = QRegExp("-?([0-9]*\\.[0-9]+)");
+    rule.format = classFormat;
+    highlightingRules.append(rule);    
+
+    /* Zeilennummern */
+    classFormat.setForeground(Qt::darkBlue);
+    classFormat.setFontWeight(QFont::Normal);
+    classFormat.setFontItalic(false);
+    rule.pattern = QRegExp("^[0-9]+:");
     rule.format = classFormat;
     highlightingRules.append(rule);
 }

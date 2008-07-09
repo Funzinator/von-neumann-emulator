52e7d464421605fc1a187072e40fb3e0bdcc87bc
Jay <sebastian.jakobi@informatik.uni-giessen.de>
2008-07-09 19:44:46 +0000

    Logische Operationen angepasst: arbeiten jetzt mit dem Integerwert, was sie ja eigentlich auch sollten.
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@112 06a71e81-df49-0410-adf8-df517167f62f

--- src/AND.cpp
+++ src/AND.cpp
@@ -8,6 +8,6 @@ AND::AND(RawOperation *rawOp) : LogicOperation(rawOp)
 void AND::run(Configuration *c)
 {
     StorageCell *p1=setP1(c);  // in p1 steht nun der "echte" Wert.
-    c->getAC()->setBinary(c->getAC()->getBinary() & p1->getBinary());
+    c->getAC()->setInt(c->getAC()->getInt() & p1->getInt());
     c->setPC(c->getPC() + 1);
 }

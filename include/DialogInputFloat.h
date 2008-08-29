c3981ce734ec58192176e10c7236ea6c9d387236
Funzinator <github@funzi.de>
2008-08-29 14:47:20 +0000

    QInputDialog durch eigene ersetzt (ermöglicht eine einheitliche Benutzerführung und macht den Code schlanker)
    git-svn-id: svn://svn.gathered-geeks.org/emulator/trunk@167 06a71e81-df49-0410-adf8-df517167f62f

---
+++ include/DialogInputFloat.h
@@ -0,0 +1,16 @@
+#ifndef DIALOGINPUTFLOAT_H_
+#define DIALOGINPUTFLOAT_H_
+
+#include <QRegExpValidator>
+
+#include "DialogInput.h"
+
+class DialogInputFloat : public DialogInput
+{
+    Q_OBJECT
+
+public:
+    DialogInputFloat(const QString &title, const QString &label, QObject *parent);
+};
+
+#endif /*DIALOGINPUTFLOAT_H_*/

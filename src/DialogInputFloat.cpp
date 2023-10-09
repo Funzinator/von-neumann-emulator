398e62bee9204fe81a79367ff227249df71bc30a
Ian Lohmann <ian@datacu.be>
2023-10-09 12:33:05 +0200

    port to qt6 (/src)

--- src/DialogInputFloat.cpp
+++ src/DialogInputFloat.cpp
@@ -2,5 +2,5 @@
 
 DialogInputFloat::DialogInputFloat(const QString &title, const QString &label, QObject *parent) : DialogInput(title, label, 0, parent)
 {
-    this->setValidator(new QRegExpValidator(QRegExp("[-+]?([0-9]*\\.?[0-9]*)"), parent));
+    this->setValidator(new QRegularExpressionValidator(QRegularExpression("[-+]?([0-9]*\\.?[0-9]*)"), parent));
 }
